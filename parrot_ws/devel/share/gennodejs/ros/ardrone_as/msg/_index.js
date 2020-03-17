7b17671c24a6061e4b1e5616ad0baf2b74ac98b5
mesneym <mesney.mesney@gmail.com>
2020-03-17 18:22:15 -0400

    Added publisher to drone

---
+++ parrot_ws/devel/share/gennodejs/ros/ardrone_as/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let ArdroneAction = require('./ArdroneAction.js');
+let ArdroneActionFeedback = require('./ArdroneActionFeedback.js');
+let ArdroneActionGoal = require('./ArdroneActionGoal.js');
+let ArdroneActionResult = require('./ArdroneActionResult.js');
+let ArdroneGoal = require('./ArdroneGoal.js');
+let ArdroneFeedback = require('./ArdroneFeedback.js');
+let ArdroneResult = require('./ArdroneResult.js');
+
+module.exports = {
+  ArdroneAction: ArdroneAction,
+  ArdroneActionFeedback: ArdroneActionFeedback,
+  ArdroneActionGoal: ArdroneActionGoal,
+  ArdroneActionResult: ArdroneActionResult,
+  ArdroneGoal: ArdroneGoal,
+  ArdroneFeedback: ArdroneFeedback,
+  ArdroneResult: ArdroneResult,
+};
