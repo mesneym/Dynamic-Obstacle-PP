12bed87d37538f0742cfcee47b24972061a04d30
Eashwar Sathyamurthy <eashwar.sathyamurthy@gmail.com>
2020-03-24 20:14:50 -0400

    Reduced the obstacle space

--- parrot_ws/devel/share/sjtu_drone/cmake/sjtu_droneConfig.cmake
+++ parrot_ws/devel/share/sjtu_drone/cmake/sjtu_droneConfig.cmake
@@ -67,14 +67,14 @@ set(sjtu_drone_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(sjtu_drone_SOURCE_PREFIX /home/eashwar/parrot_ws/src/parrot_ardrone/sjtu_drone)
-  set(sjtu_drone_DEVEL_PREFIX /home/eashwar/parrot_ws/devel)
+  set(sjtu_drone_SOURCE_PREFIX /home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/src/parrot_ardrone/sjtu_drone)
+  set(sjtu_drone_DEVEL_PREFIX /home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/devel)
   set(sjtu_drone_INSTALL_PREFIX "")
   set(sjtu_drone_PREFIX ${sjtu_drone_DEVEL_PREFIX})
 else()
   set(sjtu_drone_SOURCE_PREFIX "")
   set(sjtu_drone_DEVEL_PREFIX "")
-  set(sjtu_drone_INSTALL_PREFIX /home/eashwar/parrot_ws/install)
+  set(sjtu_drone_INSTALL_PREFIX /home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/install)
   set(sjtu_drone_PREFIX ${sjtu_drone_INSTALL_PREFIX})
 endif()
 
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(sjtu_drone_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/eashwar/parrot_ws/src/parrot_ardrone/sjtu_drone/include " STREQUAL " ")
+if(NOT "/home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/src/parrot_ardrone/sjtu_drone/include " STREQUAL " ")
   set(sjtu_drone_INCLUDE_DIRS "")
-  set(_include_dirs "/home/eashwar/parrot_ws/src/parrot_ardrone/sjtu_drone/include")
+  set(_include_dirs "/home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/src/parrot_ardrone/sjtu_drone/include")
   if(NOT " " STREQUAL " ")
     set(_report "Check the issue tracker '' and consider creating a ticket if the problem has not been reported yet.")
   elseif(NOT " " STREQUAL " ")
@@ -110,7 +110,7 @@ if(NOT "/home/eashwar/parrot_ws/src/parrot_ardrone/sjtu_drone/include " STREQUAL
         message(FATAL_ERROR "Project 'sjtu_drone' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'sjtu_drone' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/eashwar/parrot_ws/src/parrot_ardrone/sjtu_drone/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'sjtu_drone' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/src/parrot_ardrone/sjtu_drone/${idir}'.  ${_report}")
     endif()
     _list_append_unique(sjtu_drone_INCLUDE_DIRS ${include})
   endforeach()
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/eashwar/parrot_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/eashwar/Desktop/Dynamic-Obstacle-PP/parrot_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
