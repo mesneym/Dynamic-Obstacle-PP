262018238ed56fee30f44b042bc6a846dd54a7b2
mesneym <mesney.mesney@gmail.com>
2020-05-05 17:36:15 -0400

    Modified Quadrotor.py for simulation

--- code/AerialNavigation/Quadrotor.py
+++ code/AerialNavigation/Quadrotor.py
@@ -14,7 +14,7 @@ import matplotlib.animation as animation
 import mpl_toolkits.mplot3d.art3d as art3d
 
 class Quadrotor():
-    def __init__(self, x=0, y=0, z=0, roll=0, pitch=0, yaw=0, size=0.25, show_animation=True):
+    def __init__(self, ax, x=0, y=0, z=0, roll=0, pitch=0, yaw=0,size=0.25,show_animation=True):
         self.p1 = np.array([size / 2, 0, 0, 1]).T
         self.p2 = np.array([-size / 2, 0, 0, 1]).T
         self.p3 = np.array([0, size / 2, 0, 1]).T
@@ -26,13 +26,12 @@ class Quadrotor():
         self.show_animation = show_animation
 
         if self.show_animation:
-            plt.ion()
-            fig = plt.figure()
+            # plt.ion()
             # for stopping simulation with the esc key.
-            fig.canvas.mpl_connect('key_release_event',
-                    lambda event: [exit(0) if event.key == 'escape' else None])
+            # fig.canvas.mpl_connect('key_release_event',
+                    # lambda event: [exit(0) if event.key == 'escape' else None])
 
-            self.ax = fig.add_subplot(111, projection='3d')
+            self.ax = ax 
 
         self.update_pose(x, y, z, roll, pitch, yaw)
 
@@ -64,6 +63,7 @@ class Quadrotor():
              [-sin(pitch), cos(pitch) * sin(roll), cos(pitch) * cos(yaw), z]
              ])
 
+
     def plot(self):  # pragma: no cover
         T = self.transformation_matrix()
 
@@ -71,8 +71,6 @@ class Quadrotor():
         p2_t = np.matmul(T, self.p2)
         p3_t = np.matmul(T, self.p3)
         p4_t = np.matmul(T, self.p4)
-
-        plt.cla()
 
         self.ax.plot([p1_t[0], p2_t[0], p3_t[0], p4_t[0]],
                      [p1_t[1], p2_t[1], p3_t[1], p4_t[1]],
@@ -85,11 +83,7 @@ class Quadrotor():
 
         self.ax.plot(self.x_data, self.y_data, self.z_data, 'b:')
 
-        plt.xlim(-5, 5)
-        plt.ylim(-5, 5)
-        self.ax.set_zlim(0, 10)
 
-        plt.pause(0.001)
 
 
 
